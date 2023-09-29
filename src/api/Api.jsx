2a4bbc1908db2433f7b65998290cb77f0579a2c3
Sandesh Subedi <subedisandesh0123@gmail.com>
2023-09-29 17:57:36 +0545

    api changes

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -1,4 +1,23 @@
 
-let Api = "http://localhost/social/";
+// let Api = "http://localhost/social/";
+
+// export default Api;
+
+const currentURL = window.location.hostname;
+
+var isDebug = false;
+
+// export const baseUrl = `http://quizhunt.ap-south-1.elasticbeanstalk.com/`;
+//if domain is localhost then use local url
+
+console.log(currentURL);
+if (currentURL === 'localhost') {
+    isDebug = true;
+}
+
+export const debugUrl = `http://localhost/social/`;
+const Api = isDebug ? debugUrl : 'https://sbapi.quizhunt.online/';
 
 export default Api;
+
+// export const imgUrl = ``;
