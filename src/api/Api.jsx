a82996b932e56705d1bdff71117b6968a3fbe583
Sandesh Subedi <subedisandesh0123@gmail.com>
2023-10-29 18:25:25 +0545

    Api updated

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -9,8 +9,8 @@ if (currentURL === 'localhost') {
 }
 
 export const debugUrl = `http://localhost/social/`;
-export const ChatApi = isDebug ? `http://localhost:5000/api/` : 'https://chat-social-media-app.vercel.app/api/';
-export const SocketApi = isDebug ? `http://localhost:3000` : 'https://socket-chat-social-media-app-wve1.vercel.app/';
+export const ChatApi = isDebug ? `http://localhost:5000/api/` : 'https://socket-socialmediaapp.onrender.com/api/';
+export const SocketApi = isDebug ? `http://localhost:3000/` : 'https://socket-socialmediaapp.onrender.com/';
 const Api = isDebug ? debugUrl : 'https://sbapi.quizhunt.online/';
 
 export default Api;
